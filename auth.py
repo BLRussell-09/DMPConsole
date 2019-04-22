3d914a03893067cc53b594ea0c085886c6fabfea
Barry Russell <brussell@FORTERACU.COM>
2019-04-22 16:06:51 -0500

    WIP

---
+++ auth.py
@@ -0,0 +1,15 @@
+import pyrebase
+from home import get_home
+from config import config
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+def sign_in():
+  email = input("Please enter your email\n")
+  password = input("Please enter your password\n")
+  user = auth.sign_in_with_email_and_password(email, password)
+  if user:
+    get_home()
+  else:
+    exit()
